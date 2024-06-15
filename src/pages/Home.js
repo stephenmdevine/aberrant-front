d4da7c926f72ebca35600a471fa1a96052fcdd67
stephenmdevine <stephenmdevine@gmail.com>
2024-06-15 09:04:41 -0500

    Home edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Home = () => {
+
+  const [ characters, setCharacters ] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadCharacters();
+  }, []);
+
+  const loadCharacters = async () => {
+    const result = await axios.get('http://localhost:8080/characters');
+    setCharacters(result.data);
+  };
+
   return (
     <div>
       <h1>Welcome to the Character Creator</h1>
